7446c8506b48a76cfb7f3465737b7738dc846f8d
tommycor <tommycornilleau@gmail.com>
2015-11-28 05:14:31 +0100

    adding new model test

--- app/js/experiment/meshes/House.js
+++ app/js/experiment/meshes/House.js
@@ -2,7 +2,7 @@ var Emitter = require('../utils/EventEmitter');
 
 function House(){
 
-	this.url = './models/house/House_model.json';
+	this.url = './models/house/House_model-v2.json';
 	
 	this.createObject = this.createObject.bind(this);
 }
@@ -16,10 +16,11 @@ House.prototype.init = function() {
 
 House.prototype.createObject = function(geometry){
 	this.geometry = geometry;
-	this.material = new THREE.MeshLambertMaterial( {
-		color: 0x2194ce,
-		emissive: 0x000000
-	});
+	this.material = new THREE.MeshNormalMaterial({color: 0x7777ff});
+	// this.material = new THREE.MeshLambertMaterial( {
+	// 	color: 0x2194ce,
+	// 	emissive: 0x000000
+	// });
 
 	this.mesh = new THREE.Mesh(this.geometry, this.material);
 	this.mesh.name = 'house';
